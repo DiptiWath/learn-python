d9d3f9636c743f58f2c8de92dea5577aa1733aca
DiptiWath <106587979+DiptiWath@users.noreply.github.com>
2023-02-08 14:52:34 +0530

    Update shipping.py

--- 2-control-flow/sals-shipping/shipping.py
+++ 2-control-flow/sals-shipping/shipping.py
@@ -1,36 +1,33 @@
-# Sal's Shipping
-# Sonny Li
+weight = 41.5
 
-weight = 80
-
-# Ground Shipping 🚚
-
-if weight <= 2:
-  cost_ground = weight * 1.5 + 20
-elif weight <= 6:
+#"Ground Shipping"
+if weight <= 2 :
+  cost_ground = weight * 1.50 + 20
+  print("Ground Shipping : $",cost_ground )
+elif weight > 2 and weight <= 6 :
   cost_ground = weight * 3.00 + 20
-elif weight <= 10:
+  print("Ground Shipping : $",cost_ground )
+elif weight > 6 and weight <= 10 :
   cost_ground = weight * 4.00 + 20
-else:
+  print("Ground Shipping : $",cost_ground )
+elif weight > 10 :
   cost_ground = weight * 4.75 + 20
+  print("Ground Shipping : $",cost_ground )
 
-print("Ground Shipping $", cost_ground)
-      
-# Ground Shipping Premimum 🚚💨
-
+#"Ground Shipping Premium"
 cost_ground_premium = 125.00
+print("Ground Shipping Premium : $",cost_ground_premium )
 
-print("Ground Shipping Premimium $", cost_ground_premium)
-
-# Drone Shipping 🛸
-
-if weight <= 2:
-  cost_drone = weight * 4.5
-elif weight <= 6:
+#"Drone Shipping"
+if weight <= 2 :
+  cost_drone = weight * 4.50
+  print("Drone Shipping : $",cost_drone )
+elif weight > 2 and weight <= 6 :
   cost_drone = weight * 9.00
-elif weight <= 10:
+  print("Drone Shipping : $",cost_drone )
+elif weight > 6 and weight <= 10 :
   cost_drone = weight * 12.00
-else:
+  print("Drone Shipping : $",cost_drone )
+elif weight > 10 :
   cost_drone = weight * 14.25
-
-print("Drone Shipping: $", cost_drone)
+  print("Drone Shipping : $",cost_drone )
