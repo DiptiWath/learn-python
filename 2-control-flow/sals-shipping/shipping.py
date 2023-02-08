c047a73c4aad7f32828b541a50ef3627c37eab2c
DiptiWath <106587979+DiptiWath@users.noreply.github.com>
2023-02-08 15:04:47 +0530

    Update shipping.py

--- 2-control-flow/sals-shipping/shipping.py
+++ 2-control-flow/sals-shipping/shipping.py
@@ -3,13 +3,10 @@ weight = 41.5
 #"Ground Shipping"
 if weight <= 2 :
   cost_ground = weight * 1.50 + 20
-  print("Ground Shipping : $",cost_ground )
 elif weight > 2 and weight <= 6 :
   cost_ground = weight * 3.00 + 20
-  print("Ground Shipping : $",cost_ground )
 elif weight > 6 and weight <= 10 :
   cost_ground = weight * 4.00 + 20
-  print("Ground Shipping : $",cost_ground )
 elif weight > 10 :
   cost_ground = weight * 4.75 + 20
   print("Ground Shipping : $",cost_ground )
@@ -21,13 +18,10 @@ print("Ground Shipping Premium : $",cost_ground_premium )
 #"Drone Shipping"
 if weight <= 2 :
   cost_drone = weight * 4.50
-  print("Drone Shipping : $",cost_drone )
 elif weight > 2 and weight <= 6 :
   cost_drone = weight * 9.00
-  print("Drone Shipping : $",cost_drone )
 elif weight > 6 and weight <= 10 :
   cost_drone = weight * 12.00
-  print("Drone Shipping : $",cost_drone )
 elif weight > 10 :
   cost_drone = weight * 14.25
   print("Drone Shipping : $",cost_drone )
